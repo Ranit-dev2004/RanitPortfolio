6a39717ecee64ce26d216f6dc234f48ee7e037c6
Ranit-dev2004 <saharanit16@gmail.com>
2025-08-30 14:23:11 +0530

    Fix image import case for MyImage in assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,5 @@
 import Logo from "../assets/Beige Minimalist Initial Font Floral Agency Logo.png";
-import MyImage from "../assets/Myimage.JPG";
+import MyImage from "../assets/MyImage.JPG";
 export const assets = {
     Logo,
     MyImage
